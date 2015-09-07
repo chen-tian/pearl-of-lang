9f86127aaeda5843dc7443a1654f88d9a20d884a
Chen Tian <chen.tian@huawei.com>
2015-09-06 22:23:12 -0700

    add DataIOStream

---
+++ java/DataIOStream/src/main/java/DataIOStream.java
@@ -0,0 +1,33 @@
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+ 
+public class DataIOStream {
+    public static void main(String[] args) {
+        int integer  = 1000;
+        String str = "I like Java";
+        float flt = 88.88f;
+ 
+        try {
+          DataOutputStream dos = new DataOutputStream(new FileOutputStream("./target/tmp.data"));
+ 
+          dos.writeInt(integer);
+          dos.writeUTF(str);
+          dos.writeFloat(flt);
+          dos.flush();
+          dos.close();
+ 
+          DataInputStream dis = new DataInputStream(new FileInputStream("./target/tmp.data"));
+
+          int a = dis.readInt();
+          String s = dis.readUTF();
+          float f = dis.readFloat();
+          System.out.printf("Read data: %d, %s, %f%n", a, s, f);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
